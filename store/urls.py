5d799fc5c3372e8325775a6f6f3643d897e8a1ad
Apungu Raymond <63607228+Raymond-ap@users.noreply.github.com>
2021-03-23 11:24:44 -0700

    commit

--- store/urls.py
+++ store/urls.py
@@ -3,4 +3,9 @@ from .views import *
 
 urlpatterns = [
     path('', homePage, name='home'),
+    path('products', productsView, name='products'),
+    path('about', aboutView, name='about'),
+    path('checkout', checkOutView, name='checkout'),
+    path('blog', blogView, name='blog'),
+    path('detail/<slug:slug>', productDetail, name='detail'),
 ]
--- store/views.py
+++ store/views.py
@@ -1,5 +1,27 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+
 def homePage(request):
     return render(request, 'store/index.html')
+
+
+def productsView(request):
+    return render(request, 'store/product.html')
+
+
+def productDetail(request, slug):
+    return render(request, 'store/product-details.html')
+
+
+def aboutView(request):
+    return render(request, 'store/about.html')
+
+
+def checkOutView(request):
+    return render(request, 'store/checkout.html')
+
+
+def blogView(request):
+    return render(request, 'store/blog.html')
