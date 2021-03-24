d2cc7e8abd448843d46c028f449537731df169c8
Apungu Raymond <63607228+Raymond-ap@users.noreply.github.com>
2021-03-24 09:45:18 -0700

    commit

--- store/admin.py
+++ store/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from .models import Category, Product, ProductImage
 
-# Register your models here.
+
+class ProductImageInline(admin.TabularInline):
+    model = ProductImage
+    extra = 3
+
+
+class ProductAdmin(admin.ModelAdmin):
+    inlines = [ProductImageInline]
+
+
+admin.site.register(Product, ProductAdmin)
+admin.site.register(Category)
